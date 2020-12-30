a23ed6e5f913ad394ac61af7a79ed2a126e0ef57
Samuel Plumppu <greenheartmusic@gmail.com>
2020-12-30 17:57:56 +0100

    Only show upcoming events. Automatically hide past ones

--- components/Events.tsx
+++ components/Events.tsx
@@ -6,49 +6,65 @@ const Organizers = {
     Klimatprata: 'Vardagens civilkurage, genom projektet Klimatprata',
 }
 
-const events = [
+interface FutureVisionsEvent {
+    title: string
+    when: string
+    date: string
+    organizer: keyof typeof Organizers
+    link: string
+}
+
+const events: FutureVisionsEvent[] = [
     {
         title: 'Träning i visionära klimatsamtal',
         when: 'Torsdag, 21 januari kl. 17:00-19:00',
-        organizer: Organizers.Klimatprata,
+        date: '2021-01-21T16:00:00.0Z',
+        // date: '2020-12-30T16:00:00.0Z',
+        organizer: 'Klimatprata',
         link: 'https://www.facebook.com/events/787787511805460',
     },
 ]
 
-const Events = () => (
-    <>
-        <Card mx="auto" my="20" pt="4" px="2" pb="2">
-            <Heading textAlign="center" mb="2">
-                Kommande event
-            </Heading>
-            {events.map((e, i) => (
-                <Link href={e.link} isExternal key={e.link + i}>
-                    <Box
-                        borderRadius="lg"
-                        p="4"
-                        style={{
-                            textDecoration: 'none !important',
-                        }}
-                        _hover={{
-                            cursor: 'pointer',
-                            backgroundImage:
-                                'linear-gradient(25deg,#009eea,#fff)',
-                        }}
-                        position="relative"
-                    >
-                        <Heading as="h3" fontSize="md" pr="4">
-                            📅 {e.title}
-                        </Heading>
-                        <Text position="absolute" top="4" right="4">
-                            →
-                        </Text>
-                        <Text mt="2">När: {e.when}</Text>
-                        <Text>Arrangör: {e.organizer}</Text>
-                    </Box>
-                </Link>
-            ))}
-        </Card>
-    </>
-)
+const keepUpcomingEvents = (event: FutureVisionsEvent) =>
+    Date.now() <= new Date(event.date).getTime()
+
+const Events = () => {
+    const upcomingEvents = events.filter(keepUpcomingEvents)
+    return upcomingEvents.length ? (
+        <>
+            <Card mx="auto" my="20" pt="4" px="2" pb="2">
+                <Heading textAlign="center" mb="2">
+                    Kommande event
+                </Heading>
+                {events.map((e, i) => (
+                    <Link href={e.link} isExternal key={e.link + i}>
+                        <Box
+                            borderRadius="lg"
+                            p="4"
+                            style={{
+                                textDecoration: 'none !important',
+                            }}
+                            _hover={{
+                                cursor: 'pointer',
+                                backgroundImage:
+                                    'linear-gradient(25deg,#009eea,#fff)',
+                            }}
+                            position="relative"
+                        >
+                            <Heading as="h3" fontSize="md" pr="4">
+                                📅 {e.title}
+                            </Heading>
+                            <Text position="absolute" top="4" right="4">
+                                →
+                            </Text>
+                            <Text mt="2">När: {e.when}</Text>
+                            <Text>Arrangör: {Organizers[e.organizer]}</Text>
+                        </Box>
+                    </Link>
+                ))}
+            </Card>
+        </>
+    ) : null
+}
 
 export default Events
