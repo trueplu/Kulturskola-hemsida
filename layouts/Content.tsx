09bdd2c6069a4644ab0f86eb23a6712609ff38c8
Fredrik Plumppu <59374117+trueplu@users.noreply.github.com>
2021-02-08 23:40:25 +0100

    update

--- layouts/Content.tsx
+++ layouts/Content.tsx
@@ -1,7 +1,7 @@
 import { Box, BoxProps } from '@chakra-ui/react'
 
 const Content = (props: BoxProps) => (
-    <Box maxW="2xl" px="8" mx="auto" {...props} />
+    <Box maxW="2xl" px={['2', '4', '8']} mx="auto" {...props} />
 )
 
 export default Content
--- shared/constants.ts
+++ shared/constants.ts
@@ -1,5 +1,4 @@
-export const getStartedId = 'instrument'
-export const getStartedLink = `/#${getStartedId}`
+export const getStartedLink = `/ovningar`
 export const email = 'fredrik.plumppu@edu.boras.se'
 export const documentsLink =
     'https://drive.google.com/drive/folders/1C7DyNSs84Vg-m1IhAHEYD2uDxfFLC7h5?usp=sharing'
