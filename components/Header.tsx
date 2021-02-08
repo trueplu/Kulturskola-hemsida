7fc44ad45d69b6df8bea90bd6855f61e1a0eda81
Fredrik Plumppu <59374117+trueplu@users.noreply.github.com>
2021-02-08 23:27:56 +0100

    update header

--- components/Header.tsx
+++ components/Header.tsx
@@ -2,7 +2,7 @@ import { Flex, Link, Container } from '@chakra-ui/react'
 import NextLink from 'next/link'
 
 import { theme } from '../shared/theme'
-import { email, documentsLink, videosLink } from '../shared/constants'
+import { documentsLink, videosLink } from '../shared/constants'
 
 interface NavLinkProps {
     href: string
@@ -16,11 +16,11 @@ const NavLink: React.FC<NavLinkProps> = ({
 }) => (
     <NextLink href={href} passHref>
         <Link
-            py={[2, 3, 4]}
-            pt={[1, 3, 4]}
-            px={[3, 3, 4]}
+            py={[2, 3]}
+            pt={[1, 3]}
+            px={[3, 3]}
             variant="white"
-            fontSize={['lg', 'md']}
+            fontSize={['sm', null, 'md']}
             isExternal={isExternal}
         >
             {children}
@@ -41,13 +41,14 @@ const Header = () => (
         <NextLink href="/" passHref>
             <Link
                 fontFamily={theme.fonts.heading}
-                fontSize="xl"
-                px={[3, 4]}
-                py={[3, 4]}
-                pb={[1, 4]}
+                fontSize={['md']}
+                px={[2, 3]}
+                py={[2, 3]}
+                pb={[1, 3]}
                 variant="white"
+                textAlign={['center', 'left']}
             >
-                Musiklärare Fredrik Plumppu
+                Brasslärare Fredrik Plumppu
             </Link>
         </NextLink>
 
@@ -55,9 +56,12 @@ const Header = () => (
             as="nav"
             fontWeight="bold"
             justifyContent={['center', 'flex-start']}
+            flexWrap={['wrap', 'unset']}
         >
-            <NavLink href="/instrument">Instrument</NavLink>
-            <NavLink href="/orkester">Orkester</NavLink>
+            <NavLink href="/ovningar">Övningar</NavLink>
+
+            {/* <NavLink href="/orkester">Orkester</NavLink> */}
+
             <NavLink href={documentsLink} isExternal>
                 Dokument
             </NavLink>
@@ -65,7 +69,6 @@ const Header = () => (
                 Videos
             </NavLink>
             <NavLink href="/om">Om</NavLink>
-            <NavLink href={'mailto:' + email}>Kontakt</NavLink>
         </Flex>
     </Container>
 )
