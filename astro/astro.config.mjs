694547cb66ad2d3c7bf754255c5ec2c95ede569f
Joel Plumppu <16489683+paccao@users.noreply.github.com>
2024-10-20 12:34:20 +0200

    Add header, footer and images
    Co-authored-by: Samuel Plumppu <Greenheart@users.noreply.github.com>

--- astro/astro.config.mjs
+++ astro/astro.config.mjs
@@ -1,9 +1,10 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from 'astro/config'
 import tailwind from '@astrojs/tailwind'
 import mdx from '@astrojs/mdx'
 
 // https://astro.build/config
 export default defineConfig({
-    integrations: [mdx(), tailwind()]
-});
+  integrations: [mdx(), tailwind()],
+  devToolbar: { enabled: false },
+})
---
+++ astro/src/lib/constants.ts
@@ -0,0 +1,6 @@
+export const exercisesLink = `/ovningar`
+export const email = 'fredrik.plumppu@edu.boras.se'
+export const documentsLink =
+  'https://drive.google.com/drive/folders/1C7DyNSs84Vg-m1IhAHEYD2uDxfFLC7h5?usp=sharing'
+export const videosLink =
+  'https://youtube.com/playlist?list=PLRsKTJ-YEAI5BnI8795smQBpR-7ku0Imd'
